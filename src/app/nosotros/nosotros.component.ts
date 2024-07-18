829e26d65c5646f5653f0dfa694b12e06c049668
Suyin <mishelo2000@gmail.com>
2024-07-18 15:44:14 -0500

    v11

--- src/app/nosotros/nosotros.component.ts
+++ src/app/nosotros/nosotros.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { NavegacionComponent } from '../navegacion/navegacion.component';
 import { PanelFooterComponent } from '../panel-footer/panel-footer.component';
+import { NuestroEquipoComponent } from "../nuestro-equipo/nuestro-equipo.component";
+import { PilaresComponent } from '../pilares/pilares.component';
+import { ValoresComponent } from "../valores/valores.component";
 
 @Component({
   selector: 'app-nosotros',
   standalone: true,
-  imports: [NavegacionComponent,PanelFooterComponent],
+  imports: [NavegacionComponent, PanelFooterComponent, NuestroEquipoComponent, PilaresComponent, ValoresComponent],
   templateUrl: './nosotros.component.html',
   styleUrl: './nosotros.component.css'
 })
---
+++ src/app/pilares/pilares.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pilares',
+  standalone: true,
+  imports: [],
+  templateUrl: './pilares.component.html',
+  styleUrl: './pilares.component.css'
+})
+export class PilaresComponent {
+
+}
